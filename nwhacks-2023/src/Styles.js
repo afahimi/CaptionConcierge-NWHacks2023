f30498f41b0a8b70e351b97454723907511a54d6
afahimi <amin.f1000@gmail.com>
2023-01-21 14:01:09 -0800

    Amin Branch

--- nwhacks-2023/src/Styles.js
+++
@@ -1,9 +0,0 @@
-/*put imports at the top*/
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => {
-    /*put css styles here*/
-});
-
-export default useStyles;
-
