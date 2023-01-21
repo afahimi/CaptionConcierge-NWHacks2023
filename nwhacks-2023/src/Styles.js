c0097fa5ffcd5577709754a308f9c8a54d1fd4e6
Richard <nemogz@student.ubc.ca>
2023-01-21 12:38:19 -0800

    first commit

---
+++ nwhacks-2023/src/Styles.js
@@ -0,0 +1,9 @@
+/*put imports at the top*/
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => {
+    /*put css styles here*/
+});
+
+export default useStyles;
+
