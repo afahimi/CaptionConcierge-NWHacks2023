f0753b1b8f641d618f055d72a37108212d5cdcb3
afahimi <amin.f1000@gmail.com>
2023-01-21 14:42:46 -0800

    added base again

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import { useEffect } from "react";
+import useStyles from "./styles";
 
 import {
   Typography,
@@ -16,26 +17,32 @@ import {
   Container,
   Button,
   searchBar,
+  Paper,
+  withStyles,
+  makeStyles,
 } from "@mui/material";
 
 const App = () => {
+  const classes = useStyles();
   return (
     <>
-      <CssBaseline>
-        <main>
-          <Container maxWidth="md">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Enter a YouTube URL...
-            </Typography>
-          </Container>
-        </main>
-      </CssBaseline>
+      <Paper className={classes.myPaper}>
+        <CssBaseline>
+          <main>
+            <Container maxWidth="md">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Enter a YouTube URL...
+              </Typography>
+            </Container>
+          </main>
+        </CssBaseline>
+      </Paper>
     </>
   );
 };
--- webapp/src/styles.js
+++ webapp/src/styles.js
@@ -1,12 +1,14 @@
-import { CardMedia } from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import { CardMedia } from "@mui/material";
+import makeStyles from "@mui/material/styles/makeStyles";
 import { useThemeProps } from "@mui/material";
 
-
-import useStyles from "./styles";
-
 const useStyles = makeStyles((theme) => ({
-    /*insert styles here*/
+  myPaper: {
+    backgroundColor: "blue",
+  },
+  title: {
+    padding: "10px",
+  },
 }));
 
 export default useStyles;
