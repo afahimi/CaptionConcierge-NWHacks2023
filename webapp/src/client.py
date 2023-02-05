94b845f8c4afa250da4011d1b8ad4f0a0383a7f9
Tim Yang <timologize@gmail.com>
2023-02-05 13:24:47 -0800

    .

--- webapp/src/client.py
+++ webapp/src/client.py
@@ -1,7 +1,7 @@
 #Test python client for the flask server
 import requests
 import json
-
+#
 url = 'http://localhost:5000/captions'
 data = {"video_id": "mc979OhitAg"}
 headers = {'Content-type': 'application/json'}
